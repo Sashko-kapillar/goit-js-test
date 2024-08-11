610b4ad16ede7d2218f06d3c102a4ae19bb4b9a0
sashko-kapillar <kapillarua@gmail.com>
2024-08-12 00:16:40 +0300

    map & flatMap

--- js/task-5.js
+++ js/task-5.js
@@ -29,27 +29,27 @@ console.log(pointer);
 //   console.log(i);
 // });
 
-function calculateTotalPrice(orderedItems) {
-  let totalPrice = 0;
-  orderedItems.forEach(function (element) {
-    totalPrice += element;
-  });
-  return totalPrice;
-}
-console.log(calculateTotalPrice([12, 85, 37, 4]));
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   orderedItems.forEach(function (element) {
+//     totalPrice += element;
+//   });
+//   return totalPrice;
+// }
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
 
-function filterArray(numbers, value) {
-  const newArray = [];
-  numbers.forEach(function (element) {
-    if (element > value) {
-      newArray.push(element);
-      console.log(newArray);
-    }
-  });
-  return newArray;
-}
+// function filterArray(numbers, value) {
+//   const newArray = [];
+//   numbers.forEach(function (element) {
+//     if (element > value) {
+//       newArray.push(element);
+//       console.log(newArray);
+//     }
+//   });
+//   return newArray;
+// }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
 
 /* =======   СТРІЛОЧНІ ФУНКЦІЇ   ====== */
 
@@ -71,24 +71,133 @@ console.log(filterArray([1, 2, 3, 4, 5], 3));
 // }
 // console.log(calculateTotalPrice([12, 85, 37, 4]));
 
-calculateTotalPrice = (orderedItems) => {
-  let totalPrice = 0;
-  orderedItems.forEach((item) => {
-    totalPrice += item;
-  });
-  return totalPrice;
-};
-console.log(calculateTotalPrice([12, 85, 37, 4]));
+// calculateTotalPrice = (orderedItems) => {
+//   let totalPrice = 0;
+//   orderedItems.forEach((item) => {
+//     totalPrice += item;
+//   });
+//   return totalPrice;
+// };
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
 
-filterArray = (numbers, value) => {
-  const filteredNumbers = [];
+// filterArray = (numbers, value) => {
+//   const filteredNumbers = [];
+//   console.log(numbers);
+//   numbers.forEach((number) => {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+//   console.log(numbers);
+//   return filteredNumbers;
+// };
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
 
-  numbers.forEach((number) => {
-    if (number > value) {
-      filteredNumbers.push(number);
+function changeEven(numbers, value) {
+  let newArray = [];
+  numbers.forEach((element) => {
+    if (element % 2 === 0) {
+      element += value;
     }
+    newArray.push(element);
   });
+  return newArray;
+}
+console.log(changeEven([17, 24, 68, 31, 42], 100));
+console.log(changeEven([1, 2, 3, 4, 5], 10));
 
-  return filteredNumbers;
+/* =======   МЕТОДИ map ТА flatMap   ====== */
+
+const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+const planetsLengths = planets.map((planet) => planet.length);
+console.log(planetsLengths);
+
+/*  =====   end   =====  */
+
+const getUserEmails = (users) => {
+  const userEmails = users.map((user) => user.email);
+  console.log(userEmails);
+  return userEmails;
 };
-console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(getUserEmails);
+console.log(
+  getUserEmails([
+    {
+      name: "Moore Hensley",
+      email: "moorehensley@indexia.com",
+      eyeColor: "blue",
+      friends: ["Sharron Pace"],
+      isActive: false,
+      balance: 2811,
+      skills: ["ipsum", "lorem"],
+      gender: "male",
+      age: 37,
+    },
+    {
+      name: "Sharlene Bush",
+      email: "sharlenebush@tubesys.com",
+      eyeColor: "blue",
+      friends: ["Briana Decker", "Sharron Pace"],
+      isActive: true,
+      balance: 3821,
+      skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+      gender: "female",
+      age: 34,
+    },
+    {
+      name: "Ross Vazquez",
+      email: "rossvazquez@xinware.com",
+      eyeColor: "green",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+      isActive: false,
+      balance: 3793,
+      skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+      gender: "male",
+      age: 24,
+    },
+    {
+      name: "Elma Head",
+      email: "elmahead@omatom.com",
+      eyeColor: "green",
+      friends: ["Goldie Gentry", "Aisha Tran"],
+      isActive: true,
+      balance: 2278,
+      skills: ["adipisicing", "irure", "velit"],
+      gender: "female",
+      age: 21,
+    },
+    {
+      name: "Carey Barr",
+      email: "careybarr@nurali.com",
+      eyeColor: "blue",
+      friends: ["Jordan Sampson", "Eddie Strong"],
+      isActive: true,
+      balance: 3951,
+      skills: ["ex", "culpa", "nostrud"],
+      gender: "male",
+      age: 27,
+    },
+    {
+      name: "Blackburn Dotson",
+      email: "blackburndotson@furnigeer.com",
+      eyeColor: "brown",
+      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      isActive: false,
+      balance: 1498,
+      skills: ["non", "amet", "ipsum"],
+      gender: "male",
+      age: 38,
+    },
+    {
+      name: "Sheree Anthony",
+      email: "shereeanthony@kog.com",
+      eyeColor: "brown",
+      friends: ["Goldie Gentry", "Briana Decker"],
+      isActive: true,
+      balance: 2764,
+      skills: ["lorem", "veniam", "culpa"],
+      gender: "female",
+      age: 39,
+    },
+  ])
+);
