ae90f9bf79b0385d5a1e9eb8dc566d837c2c160c
sashko-kapillar <kapillarua@gmail.com>
2024-08-27 13:53:46 +0300

    metod endsWith in task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-function getShippingMessage(country, price, deliveryFee){
+function getShippingMessage(country, price, deliveryFee) {
   const totalPrice = price + deliveryFee;
   return `You ordered ${country} droids worth ${totalPrice} credits!`;
 }
@@ -8,3 +8,12 @@ function getShippingMessage(country, price, deliveryFee){
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
 console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+function checkFileExtension(fileName, ext) {
+  if (fileName.endsWith(ext)) {
+    return "File extension matches";
+  } else {
+    return "File extension does not match";
+  }
+}
+console.log(checkFileExtension("styles.css", ".js"));
